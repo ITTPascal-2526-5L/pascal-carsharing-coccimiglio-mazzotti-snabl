8e0ff2ca4f16c3d56973798a75d5b759a2ac0c17
DuoFresh <ccoccimiglio5@gmail.com>
2025-10-23 13:46:44 +0200

    api

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
-
 def home():
     return "<h1>Hello World</h1>"
+
+
+@app.route('/<string:name>', methods=['GET'])
+def greet(name: str):
+    return f"<h1>Hello {name}</h1>"
+
+
+@app.route('/api/<path:path>', methods=['GET','POST','PUT','DELETE'])
+def api_route(path):
+    return f"<h1>API endpoint: {path}</h1>"
+
+
 app.run()

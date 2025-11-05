95b58fd3eef25e7578178b08f0d9492da6789d9b
snabls <samuele.snabl.stud@ispascalcomandini.it>
2025-11-05 13:02:34 +0100

    Rospo pt 3

--- app/routes/register.py
+++ app/routes/register.py
@@ -2,11 +2,15 @@ from flask import Blueprint, render_template, request, redirect, url_for
 
 register_bp = Blueprint("register", __name__)
 
-@register_bp.route("/register_driver")
+@register_bp.route("/register_driver", methods=["POST"])
 def register_driver():
+    if (request.method == "POST"):
+        print("POST")
     return render_template("register.html")
 
 
-@register_bp.route("/register_passenger")
+@register_bp.route("/register_passenger", methods=["POST"])
 def register_passenger():
+    if (request.method == "POST"):
+        print("POST")
     return render_template("register.html")
